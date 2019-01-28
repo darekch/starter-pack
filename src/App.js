095ec8473a875871f9b15de289619a9c41726460
darekch <darek.chec@gmail.com>
2019-01-28 23:11:41 +0100

    Dependencies have been updated and added @babel/react preset.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React, { Component} from "react";
+import React, { Component } from "react";
 
 import "./App.css";
 
-
 class App extends Component {
-
   render() {
-    return(
+    return (
       <div className="App">
-        <h1> Hello, World! </h1>
+        <h1> Hello, World!</h1>
       </div>
     );
   }
